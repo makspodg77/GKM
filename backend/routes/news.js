b45cd29b31bb06fbd1070ffb4dfdaf447d7c922f
Maksymilian Podgórski <makspodg77@gmail.com>
2024-12-20 21:17:38 +0100

    Log connection string in news route for debugging purposes

--- backend/routes/news.js
+++ backend/routes/news.js
@@ -2,6 +2,7 @@ const express = require("express");
 const sql = require("msnodesqlv8");
 const config = require("../utils/config");
 const router = express.Router();
+console.log(config.CONNECTION_STRING);
 router.get("/", (req, res) => {
   const query = `
     SELECT * FROM news`;
