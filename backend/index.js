63ff0840a0c073a42eee707551b4028e39d7193a
Maksymilian Podgórski <makspodg77@gmail.com>
2025-01-31 22:34:52 +0100

    Refactor static file serving in backend to improve path handling and simplify code

--- backend/index.js
+++ backend/index.js
@@ -14,28 +14,24 @@ const port = 8080;
 app.use(cors());
 app.use(express.json());
 
-// API routes
 app.use("/api/timetable", timetableRouter);
 app.use("/api/routes", routesRouter);
 app.use("/api/transportLines", transportLinesRouter);
 app.use("/api/transportStops", transportStopsRouter);
 app.use("/api/news", newsRouter);
 
-// Serve static frontend files
-app.use(express.static(path.join(__dirname, "frontend", "dist")));
+const frontendPath = path.join(__dirname, "..", "frontend", "dist");
 
-// If no API route or static file matches, serve the frontend index.html
+app.use(express.static(frontendPath));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.join(frontendPath, "index.html"));
 });
 
-// Health check endpoint
 app.get("/health", (req, res) => {
   console.log("Health check endpoint hit");
   res.status(200).send("OK");
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
